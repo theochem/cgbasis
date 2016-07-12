theochem/horton

Added modified 2-electron integrals, auxiliary functions and tests

horton/gbasis/ints.h
@@ -97,7 +97,7 @@ typedef struct {
     double alpha;
 } libint_arg_t;
 
-class GB4ElectronRepulsionIntegralLibInt : public GB4Integral {
+class GB4LibInt : public GB4Integral {
     private:
         Libint_eri_t erieval;
         libint_arg_t libint_args[4];
@@ -105,10 +105,48 @@ class GB4ElectronRepulsionIntegralLibInt : public GB4Integral {
         double ab[3], cd[3];
         double ab2, cd2;
     public:
-        GB4ElectronRepulsionIntegralLibInt(long max_shell_type);
-        ~GB4ElectronRepulsionIntegralLibInt();
+        GB4LibInt(long max_shell_type);
+        ~GB4LibInt();
         virtual void reset(long shell_type0, long shell_type1, long shell_type2, long shell_type3, const double* r0, const double* r1, const double* r2, const double* r3);
         virtual void add(double coeff, double alpha0, double alpha1, double alpha2, double alpha3, const double* scales0, const double* scales1, const double* scales2, const double* scales3);
+        virtual void laplace_of_potential(double prefac, double rho, double t, long mmax, double* output) = 0;
+    };
+
+
+class GB4ElectronRepulsionIntegralLibInt : public GB4LibInt {
+    public:
+        GB4ElectronRepulsionIntegralLibInt(long max_shell_type) : GB4LibInt(max_shell_type) {};
+        virtual void laplace_of_potential(double prefac, double rho, double t, long mmax, double* output);
+    };
+
+
+class GB4ErfIntegralLibInt : public GB4LibInt {
+    private:
+        double mu;
+    public:
+        GB4ErfIntegralLibInt(long max_shell_type, double mu) : GB4LibInt(max_shell_type), mu(mu) {};
+        virtual void laplace_of_potential(double prefac, double rho, double t, long mmax, double* output);
+        const double get_mu() const {return mu;};
+    };
+
+class GB4GaussIntegralLibInt : public GB4LibInt {
+    private:
+        double c;
+        double galpha;
+    public:
+        GB4GaussIntegralLibInt(long max_shell_type, double c, double galpha) : GB4LibInt(max_shell_type), c(c), galpha(galpha) {};
+        virtual void laplace_of_potential(double prefac, double rho, double t, long mmax, double* output);
+        const double get_c() const {return c;};
+        const double get_galpha() const {return galpha;};
+    };
+
+class GB4RAlphaIntegralLibInt : public GB4LibInt {
+    private:
+        double ralpha;
+    public:
+        GB4RAlphaIntegralLibInt(long max_shell_type, double ralpha) : GB4LibInt(max_shell_type), ralpha(ralpha) {};
+        virtual void laplace_of_potential(double prefac, double rho, double t, long mmax, double* output);
+        const double get_ralpha() const {return ralpha;};
     };
 
 

theochem/cgbasis

ints.h
@@ -364,4 +364,25 @@ class GB4ErfIntegralLibInt : public GB4IntegralLibInt {
 };
 
 
+class GB4GaussIntegralLibInt : public GB4IntegralLibInt {
+    private:
+        double c;
+        double galpha;
+    public:
+        GB4GaussIntegralLibInt(long max_shell_type, double c, double galpha) : GB4IntegralLibInt(max_shell_type), c(c), galpha(galpha) {};
+        virtual void laplace_of_potential(double prefac, double rho, double t, long mmax, double* output);
+        const double get_c() const {return c;};
+        const double get_galpha() const {return galpha;};
+    };
+
+class GB4RAlphaIntegralLibInt : public GB4IntegralLibInt {
+    private:
+        double ralpha;
+    public:
+        GB4RAlphaIntegralLibInt(long max_shell_type, double ralpha) : GB4IntegralLibInt(max_shell_type), ralpha(ralpha) {};
+        virtual void laplace_of_potential(double prefac, double rho, double t, long mmax, double* output);
+        const double get_ralpha() const {return ralpha;};
+    };
+
+
 #endif